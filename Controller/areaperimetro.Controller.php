c5b2920b4649430b919082e9f945e2a34407cffe
Javise46 <javierserrano2000@gmail.com>
2022-04-28 16:36:59 -0600

    a

--- Controller/areaperimetro.Controller.php
+++ Controller/areaperimetro.Controller.php
@@ -11,14 +11,12 @@
         {
           
             $total = 0;
+            $total2 = 0;
                 $num1=$_POST['n1'];
                 $num2=$_POST['n2'];
                 $operacion=$_POST['operacion'];
-                    if($operacion == 'Rectangulo' || $operacion == 'Cuadrado' )
-                    {
                         $total= ($num1 + $num2)*2;
                         $total2 = ($num1 * $num2);
-                    }
                     $this->smarty->assign('respuesta',$total);
                     $this->smarty->assign('respuesta1',$total2);
                     $this->smarty->assign('title','Resultado');
